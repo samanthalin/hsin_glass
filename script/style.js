752064eca2918f97b8f8727801d3376d4be237f6
samantha lin <samanthalin1992@gmail.com>
2017-08-16 11:11:22 +0800

    done

--- script/style.js
+++ script/style.js
@@ -3,4 +3,30 @@ $(document).ready(function(){
 		$('.menu a').removeClass('active');
 		$(this).addClass('active');
 	});
+
+	$('.menu-item').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 300);
+    });
+
+	$('a[href*="#history"]').click(function(){
+		$('#process').hide();
+		$('#history').show();
+		var h = $('#history').height();
+		var nh = $('nav').height();
+		var bh = $('.banner').height();
+		$('.track').height(h + nh + bh);
+	});
+
+	$('a[href*="#process"]').click(function(){
+		$('#history').hide();
+		$('#process').show();
+		var h = $('#process').height();
+		var nh = $('nav').height();
+		var bh = $('.banner').height();
+		$('.track').height(h + nh + bh);
+	});
+
 });
