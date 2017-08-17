3a7186f57c392cb5a2927fd50b54497c4cefb0d7
ookangzheng-contribution <okzmdu@gmail.com>
2017-08-17 22:43:40 +0800

    lol

--- script/style.js
+++ script/style.js
@@ -1,32 +1,45 @@
-$(document).ready(function(){
-	$('.menu-item').click(function(){
+$(document).ready(function () {
+	var controller = new ScrollMagic.Controller();
+	$('.menu-item').click(function () {
 		$('.menu a').removeClass('active');
 		$(this).addClass('active');
 	});
 
 	$('.menu-item').on('click', function (e) {
-        e.preventDefault();
-        $('html,body').animate({
-            scrollTop: 0
-        }, 300);
-    });
+		e.preventDefault();
+		$('html,body').animate({
+			scrollTop: 0
+		}, 300);
+	});
 
-	$('a[href*="#history"]').click(function(){
+	$('a[href*="#history"]').click(function () {
 		$('#process').hide();
 		$('#history').show();
 		var h = $('#history').height();
 		var nh = $('nav').height();
 		var bh = $('.banner').height();
 		$('.track').height(h + nh + bh);
+
 	});
 
-	$('a[href*="#process"]').click(function(){
+	$('a[href*="#process"]').click(function () {
 		$('#history').hide();
 		$('#process').show();
 		var h = $('#process').height();
 		var nh = $('nav').height();
 		var bh = $('.banner').height();
 		$('.track').height(h + nh + bh);
+		// $('.content .left').hide();
+		// $('.content .right').hide();
+		$('video').addClass('animated');
+		var scene1 = new ScrollMagic.Scene({ triggerElement: "#trigger1", duration: 450 })
+			// animate color and top border in relation to scroll position
+			.setClassToggle("#trigger1", "slideIn")
+			.addIndicators()
+			.addTo(controller);
+		// $('video').addClass('fadeIn')
 	});
+
+
 
 });
