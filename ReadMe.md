12d27c59b6d50477a7d38bc30220471034cc32b0
ookangzheng-contribution <okzmdu@gmail.com>
2017-08-22 18:28:41 +0800

    fix bug

--- ReadMe.md
+++ ReadMe.md
@@ -4,9 +4,10 @@
 1. With Node server installed
 2. Start with 
     ```js 
-    npm start
+    node app
     ```
-3. Dev server will start on port 80
+3. Dev server will start on port 8080
+
 參考資料: 玻璃工藝博物館
 
 關鍵字: 玻璃, 藝術, 歷史, 文化, 博物館, 新竹
--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var static = require('node-static');
 
-var fileServer = new static.Server('./');
+var fileServer = new static.Server('./public');
 
-require('http').createServer(function(request, response) {
-    request.addListener('end', function() {
-        fileServer.serve(request, response);
-    }).resume();
-}).listen(process.env.PORT ||80);
+require('http').createServer(function (request, response) {
+request.addListener('end', function () {
+    fileServer.serve(request, response, function (err, result) {
+        if (err) { // There was an error serving the file 
+            console.error("Error serving " + request.url + " - " + err.message);
+
+            // Respond to the client 
+            response.writeHead(err.status, err.headers);
+            response.end();
+        }
+    });
+}).resume();
+}).listen(8080);
